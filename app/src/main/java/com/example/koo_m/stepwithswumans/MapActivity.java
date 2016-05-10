8a2eb51ba9fd20a0c6fb53f29a7fcb1162313de3
Koojahyun <kjh930819@naver.com>
2016-05-10 19:08:53 +0900

    Second

--- app/src/main/java/com/example/koo_m/stepwithswumans/MapActivity.java
+++ app/src/main/java/com/example/koo_m/stepwithswumans/MapActivity.java
@@ -1,10 +1,13 @@
 package com.example.koo_m.stepwithswumans;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -39,6 +42,29 @@ public class MapActivity extends AppCompatActivity{
                 map1.setVisibility(View.VISIBLE);
                     }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(getApplicationContext(),SettingsActivity.class);
+            startActivity(intent);
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
