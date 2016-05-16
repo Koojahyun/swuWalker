cc715f95080076072ef6b95da54c75d738316809
Koojahyun <kjh930819@naver.com>
2016-05-17 06:45:10 +0900

    step counter

--- app/src/main/java/com/example/koo_m/stepwithswumans/LoginActivity.java
+++ app/src/main/java/com/example/koo_m/stepwithswumans/LoginActivity.java
@@ -24,7 +24,6 @@ public class LoginActivity extends AppCompatActivity {
 
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
-            finish();
     }
 
 
--- app/src/main/java/com/example/koo_m/stepwithswumans/MainActivity.java
+++ app/src/main/java/com/example/koo_m/stepwithswumans/MainActivity.java
@@ -86,18 +86,9 @@ public class MainActivity extends AppCompatActivity {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-            //여기서부터 설명 시작!
-            //fragmentManager 의 backStack 에는 지금까지 우리가 열어본 fragment 들이 차곡차곡 싸였있음!
-            //그럼으로 backStack 에 싸여있는 fragment 가 0 이상일때 back 버튼을 누르면 밑에있는 코드가 실행됩니다 (한번만 프래그먼트를 실행했으면 fragmentManager.getBackStackEntryCount() 는 1, 두번했으면 2 등등)
         } else if (fragmentManager.getBackStackEntryCount() > 0) {
-            //backStack 이 0보다 컸기때문에 밑에있는 3줄을 실행!
-            //fragmentManager.getBackStackEntryCount() 는 현제 몇게의 fragment 가 싸여있는지 보여줍니다, 그러니까 그것에서 1을 빼면 바로 전 fragment 가 있는 위치!
-            //int backStack 에 그 위치를 저장합니다.
             int backStack = fragmentManager.getBackStackEntryCount() - 1;
-            //이제 그 위치에 있는 fragment 를 가져옵니다. (fragmentManager.getFragment().get(backStack)
-            //가져온 fragment 에 있는 .onResume() function 을 실행해주면 끝~~!!! (지금 각 fragment 의 onResume()에는 setTitle 과 타이틀의 이름을 Toast 로 보여주는 코드, 그리고 drawer 의 아이템 선택이 사용되고있습니다)
             fragmentManager.getFragments().get(backStack).onResume();
-            //이제 다했으니까 가장 위에있는 fragment 를 지워줍니다~!
             fragmentManager.popBackStack();
         } else {
             super.onBackPressed();
