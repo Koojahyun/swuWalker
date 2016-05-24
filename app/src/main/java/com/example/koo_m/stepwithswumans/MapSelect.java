2ebae2efabcd6b479009e0faacc94fb90533dd37
bl1987 <bl1987korea@naver.com>
2016-05-24 19:11:12 -0400

    지도 array 시작

---
+++ app/src/main/java/com/example/koo_m/stepwithswumans/MapSelect.java
@@ -0,0 +1,9 @@
+package com.example.koo_m.stepwithswumans;
+
+
+import android.view.View;
+import android.widget.ImageView;
+
+public class MapSelect{
+    int[][] loc = {{R.drawable.swsm_mapclass01,R.drawable.swsm_mapclass02,R.drawable.swsm_mapclass03,R.drawable.swsm_mapclass04,R.drawable.swsm_mapclass05,R.drawable.swsm_mapclass06,R.drawable.swsm_mapclass07,R.drawable.swsm_mapclass08,R.drawable.swsm_mapclass09,R.drawable.swsm_mapclass10,R.drawable.swsm_mapclass11},{}};
+}
