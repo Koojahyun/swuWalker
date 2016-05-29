4f59e23e565cbdd6bcaa7f935d80813246e02891
Koojahyun <kjh930819@naver.com>
2016-05-30 07:51:03 +0900

    Text View 추가

--- app/src/main/java/com/example/koo_m/stepwithswumans/Database.java
+++ app/src/main/java/com/example/koo_m/stepwithswumans/Database.java
@@ -7,12 +7,12 @@ import android.provider.BaseColumns;
  */
 public final class Database {
     public static final class CreateDB implements BaseColumns {
-        //        public static final String YEAR = "year";
+//        public static final String YEAR = "year";
 //        public static final String MONTH = "month";
 //        public static final String DAY = "day";
         public static final String DATE = "date";
         public static final String COUNT = "count";
         public static final String TABLENAME = "records";
-        public static final String CREATE = "create table" + TABLENAME + "(" + _ID + " integer primary key autoincrement, " + DATE + " date not null , " + COUNT + " integer not null );";
+        public static final String CREATE = "create table" + TABLENAME + "(" + _ID + " integer primary key autoincrement, " + DATE + " date not null, " + COUNT + " integer not null );";
     }
 }
