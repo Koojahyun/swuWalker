13f3473cd3cede2934a8fe283a7a5aae3ca9ffc2
bl1987 <bl1987korea@naver.com>
2016-06-17 18:07:00 -0400

    Fragment3을 level 텝으로 만듬

--- app/src/main/java/com/example/koo_m/stepwithswumans/Fragment3.java
+++ app/src/main/java/com/example/koo_m/stepwithswumans/Fragment3.java
@@ -1,6 +1,7 @@
 package com.example.koo_m.stepwithswumans;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -8,10 +9,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Fragment3 extends Fragment {
     NavigationView nv;
+    TextView levelVal;
+    TextView totalStep;
+    ImageView character;
 
     public Fragment3() {
     }
@@ -21,6 +27,15 @@ public class Fragment3 extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fragment3, container, false);
         nv = (NavigationView) getActivity().findViewById(R.id.nav_view);
+        levelVal = (TextView) view.findViewById(R.id.levelValue);
+        totalStep = (TextView) view.findViewById(R.id.totalWalkCountVal);
+        character = (ImageView) view.findViewById(R.id.character);
+
+        Cursor resultSet = MainActivity.mDatabase.rawQuery("SELECT SUM(COUNT) FROM WALK;", null);
+        resultSet.moveToFirst();
+        int totalSteps = Integer.parseInt(resultSet.getString(0));
+        totalStep.setText(totalSteps);
+
         return view;
     }
 
